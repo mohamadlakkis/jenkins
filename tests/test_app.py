b6b8ef011ef4082de163e6d07330fb7143ea1fcd
mohamadlakkis <mohamad.allakkis@gmail.com>
2024-11-11 17:23:08 +0300

    .gitignore

--- tests/test_app.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from app import greet 
-def test_greet(self):
-    self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
-if __name__ == "__main__":
-    unittest.main()
