bc77b5e54a079610025e685c75a8aafebb6b94ee
mohamadlakkis <mohamad.allakkis@gmail.com>
2024-11-11 18:20:50 +0300

    Two lines space

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,5 +2,7 @@ import unittest
 from app import greet 
 def test_greet(self):
     self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+
+    
 if __name__ == "__main__":
     unittest.main()
