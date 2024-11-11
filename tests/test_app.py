97e8dfa72f473160099f155f02c5b42de25b2165
mohamadlakkis <mohamad.allakkis@gmail.com>
2024-11-11 17:25:23 +0300

    First Inital Commit

---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+import unittest
+from app import greet 
+def test_greet(self):
+    self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+if __name__ == "__main__":
+    unittest.main()
